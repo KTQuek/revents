5aec5c6786aeafb1fdb2af8232d1aaf4ba2c5484
KTQuek <qkt7777@gmail.com>
2019-10-18 15:44:57 +0800

    L38 ComponentDidMount for the Form component

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -5,7 +5,7 @@ import EventListAttendee from './EventListAttendee';
 
 class EventListItem extends Component {
 	render() {
-		const {event} = this.props;
+		const {event, selectEvent} = this.props;
 		return (
 			<Segment.Group>
 			<Segment>
@@ -36,7 +36,12 @@ class EventListItem extends Component {
 			</Segment>
 			<Segment clearing>
 			  <span>{event.description}</span>
-			  <Button as='' color="teal" floated="right" content="View" />
+			  <Button 
+				  onClick={() => selectEvent(event)}
+				  as='' color="teal" 
+				  floated="right" 
+				  content="View" 
+				  />
 			</Segment>
 		  </Segment.Group>
 			
