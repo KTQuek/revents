93c0997f71c0168b38de78a851120b939f10e638
KTQuek <qkt7777@gmail.com>
2019-10-28 15:57:05 +0800

    L52,53,54 Redux store conf, Reducer, Store, Provider and MapStateToProps

---
+++ src/app/store/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore } from "redux";
+import testReducer from "../../features/testarea/testReducer";
+
+
+export const configureStore = () => {
+	const store = createStore(testReducer)
+
+	return store;
+}
